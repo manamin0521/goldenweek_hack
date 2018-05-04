f38c7161fb8d99843b84e9515bd4a4ef0ef51636
matsubara.manami <manami.hr.0521@gmail.com>
2018-05-04 15:11:09 +0900

    Can image upload

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180504020341) do
+ActiveRecord::Schema.define(version: 20180504053000) do
 
   create_table "posts", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20180504020341) do
     t.text "tec"
     t.text "point"
     t.text "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "image"
+  end
+
+  create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
