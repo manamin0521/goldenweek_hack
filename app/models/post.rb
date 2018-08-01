45a71df88b63cc93660d92135c1049c8a17871f4
matsubara.manami <manami.hr.0521@gmail.com>
2018-08-01 22:20:08 +0900

    Add validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
+  validates :name, presence: true
+  validates :image, presence: true
+  validates :body, presence: true 
   belongs_to :user, optional: true
 end
